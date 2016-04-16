9b281853fc8d00383eeb1685e4cac215e277c1fa
Niko <niko.nyrhila@gmail.com>
2016-04-16 19:01:32 +0300

    Basic scripts

---
+++ installDocker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://sreeninet.wordpress.com/2016/02/21/docker-on-raspberry-pi/
+
+curl -sSL http://downloads.hypriot.com/docker-hypriot_1.10.1-1_armhf.deb >/tmp/docker-hypriot_1.10.1-1_armhf.deb
+sudo dpkg -i /tmp/docker-hypriot_1.10.1-1_armhf.deb
+rm -f /tmp/docker-hypriot_1.10.1-1_armhf.deb
+sudo sh -c 'usermod -aG docker $SUDO_USER'
+sudo systemctl enable docker.service
